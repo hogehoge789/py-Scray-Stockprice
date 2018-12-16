75fb031cdca8e78156f7ec995a3c584c047e4f8d
hogehoge789 <hogehoge789@gmail.com>
2018-12-16 12:25:37 +0900

    追記

--- title_scraping.py
+++ title_scraping.py
@@ -2,6 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get('https://www.scrapmagazine.com/')
-soup = BeautifulSoup(response.text,'lxml')
+soup = BeautifulSoup(response.text, 'lxml')
 title = soup.title.string
 print(title)
